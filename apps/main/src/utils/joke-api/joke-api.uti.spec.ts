0707e20fd75794258bad0d87c14eb10c2b9fbf59
fleish80 <Fleish.D@gmail.com>
2023-09-23 22:26:23 +0300

    angular test inject hook

---
+++ apps/main/src/utils/joke-api/joke-api.uti.spec.ts
@@ -0,0 +1,37 @@
+import { JokeApiUtil, JOKE_URL } from './joke-api.util';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { Joke } from '../../models/joke.model';
+
+describe('JokeApiUtil', () => {
+  let httpTestingController: HttpTestingController;
+  let jokeApiUtil: () => Promise<Joke>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [JokeApiUtil],
+    });
+
+    httpTestingController = TestBed.inject(HttpTestingController);
+    jokeApiUtil = TestBed.inject(JokeApiUtil);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should return a joke', async () => {
+    const mockJoke: Joke = {
+      value: 'Chuck Norris can divide by zero.',
+    };
+
+    jokeApiUtil().then((joke) => {
+      expect(joke).toEqual(mockJoke);
+    });
+
+    const req = httpTestingController.expectOne(JOKE_URL);
+    expect(req.request.method).toEqual('GET');
+    req.flush(mockJoke);
+  });
+});
